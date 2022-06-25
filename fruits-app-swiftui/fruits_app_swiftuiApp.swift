8abcd78e2a13fa4d23c3a6fc4435c8ff15677d54
Charles Roberts <cjroberts2013@gmail.com>
2022-06-25 01:20:56 -0400

    Initial Commit

---
+++ fruits-app-swiftui/fruits_app_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  fruits_app_swiftuiApp.swift
+//  fruits-app-swiftui
+//
+//  Created by Charles Roberts on 6/25/22.
+//
+
+import SwiftUI
+
+@main
+struct fruits_app_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
